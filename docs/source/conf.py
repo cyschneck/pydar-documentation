615e5c163c32d9083cdd5116379c3994b8ad51bb
Cora Y. Schneck <22159116+cyschneck@users.noreply.github.com>
2023-01-27 23:12:36 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Pydar'
+copyright = '2022'
+author = 'Schneck'
 
 release = '0.1'
 version = '0.1.0'
